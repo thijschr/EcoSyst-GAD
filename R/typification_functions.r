801422974c90a14c0466e6e1ed36eeafac8d273c
Thijs van Son <thijs.vanson@runbox.com>
2020-11-17 08:51:51 +0100

    Changing to new naming conventions

--- R/typification_functions.r
+++ R/typification_functions.r
@@ -3,34 +3,54 @@
 # Olav Skarpaas and Thijs Christiaan van Son, GEco, NHM, UiO
 
 ## Simulate species data
-sim.sp <- function(lce.len = 100,      # length of lce
-                   n.sp = 10)          # number of species
+sim.sp <- function(lcelen = 100,      # length of lce
+                   numsp = 10)          # number of species
 {
-  lce <- 1:lce.len
-  nsp <- n.sp
+  lce <- 1:lcelen
+  nsp <- numsp
   
   ## Means of species distributions along the LCE
-  sp.mn1 <- sort(runif(nsp, 
+  sp_mn1 <- sort(runif(nsp, 
                        min(lce), 
                        max(lce)))
-  sp.mn2 <- sort(runif(nsp,
+  sp_mn2 <- sort(runif(nsp,
                        quantile(lce, 0.25),
                        quantile(lce, 0.75)))
-  sp.mn3 <- floor(max(lce) / nsp) * (1:nsp)
+  sp_mn3 <- floor(max(lce) / nsp) * (1:nsp)
   
   ## SD of species distributions along the LCE
-  sp.sd1 <- rep(max(lce) / 10, nsp) 
-  sp.sd2 <- runif(nsp, 
+  sp_sd1 <- rep(max(lce) / 10, nsp) 
+  sp_sd2 <- runif(nsp, 
                   max(lce) / 8, 
                   max(lce) / 2)
-  sp.sd3 <- runif(nsp,
+  sp_sd3 <- runif(nsp,
                   max(lce) / 10, 
                   max(lce) / 3)
   
   ## Simulated abundance along LCE (rows) for each species (cols)
-  sp.abun <- mapply(dnorm,
-                    mean = sp.mn1,
-                    sd = sp.sd1,
+  sp_abun <- mapply(dnorm,
+                    mean = sp_mn1,
+                    sd = sp_sd1,
                     MoreArgs = list(x = lce))
-  sp.abun
+  sp_abun
 }
+
+
+## Plot simulated species
+plt.sim.sp <- function(n.sp = 10,      # Number of simulated species
+                       sim.abun)        # Simulated abundances
+{
+  ## Defining species colours
+  sp.col <- tim.colors(n.sp)
+  par(mfrow = c(1, 1))
+  par(mar = c(2, 2, 1, 1),
+      mgp = c(0.5, 0.5, 0))
+  plot()
+}
+
+sp.col <- tim.colors(n.sp)
+par(mfrow=c(1,1))
+par(mar=c(2,2,1,1),mgp=c(0.5,0.5,0))
+plot(y1[,1],type="l",bty="l",xaxt="n",yaxt="n",ylim=c(0,1.4*max(y1)),col=sp.col[1],
+     xlab="Key source of variation (complex-variable)",ylab="Key characteristic")
+for(i in 2:n.sp) lines(y1[,i],col=sp.col[i])
