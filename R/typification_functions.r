8c87cb32a39f9990c8f6256d3abdf414b954a2e1
Thijs van Son <thijs.vanson@runbox.com>
2020-11-18 10:16:10 +0100

    Change naming style to comply with tidyverse style guide

--- R/typification_functions.r
+++ R/typification_functions.r
@@ -3,32 +3,32 @@
 # Olav Skarpaas and Thijs Christiaan van Son, GEco, NHM, UiO
 
 ## Simulate species data
-sim.sp <- function(lcelen = 100,      # length of lce
+sim_sp <- function(lcelen = 100,      # length of lce
                    numsp = 10)          # number of species
 {
   lce <- 1:lcelen
   nsp <- numsp
   
   ## Means of species distributions along the LCE
-  sp_mn <- sort(runif(nsp, 
-                      min(lce), 
-                      max(lce)))
+  mn <- sort(runif(nsp, 
+                   min(lce), 
+                   max(lce)))
   
   
   ## SD of species distributions along the LCE
-  sp_sd <- runif(nsp, 
-                 max(lce) / 25, 
-                 max(lce) / 4)
+  std <- runif(nsp, 
+               max(lce) / 25, 
+               max(lce) / 4)
   
   
   ## Simulated abundance along LCE (rows) for each species (cols)
-  sp_abun <- mapply(dnorm,
-                    mean = sp_mn,
-                    sd = sp_sd,
-                    MoreArgs = list(x = lce))
+  abun <- mapply(dnorm,
+                 mean = mn,
+                 sd = std,
+                 MoreArgs = list(x = lce))
   
-  list(sp_abun = sp_abun,
-       sp_mn = sp_mn,
-       sp_sd = sp_sd)
+  list(sp_abun = abun,
+       sp_mn = mn,
+       sp_sd = std)
 }
 
