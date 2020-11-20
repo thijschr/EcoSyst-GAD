3e688b0aee3708fc5b93c4e8bae9f92cfe8183a8
Thijs van Son <thijs.vanson@runbox.com>
2020-11-20 13:53:47 +0100

    Added functions for creating GADs from sim species

--- R/typification_functions.r
+++ R/typification_functions.r
@@ -4,7 +4,7 @@
 
 ## Simulate species data
 sim_sp <- function(lcelen = 100,      # length of lce
-                   numsp = 10)          # number of species
+                   numsp = 10)        # number of species
 {
   lce <- 1:lcelen
   nsp <- numsp
@@ -32,3 +32,20 @@ sim_sp <- function(lcelen = 100,      # length of lce
        sp_sd = std)
 }
 
+
+## Transforming simulated species data to standardised 7-step scale
+transf_seven_scale <- function(abund) {
+  as.numeric(cut(abund,
+                 breaks = c(0, 1/64, 1/16, 1/4, 3/4, 1))) + 
+    as.numeric(abund > 3/4 & mean(abund) > 1/8)
+}
+
+## Creation of generalised species data from simulated species data
+# Simulated species data need to be standardised (values between 0 and 1).
+# The standardised value then represent "konstans", which can be translated to standardised abundances
+# Then, at each observation point with a candidate type, generate generalised species lists.
+gen_sp_lst <- function(simabun) {
+  abun_stand <- simabun / max(simabun)
+  gad <- apply(X = abun_stand, MARGIN = 2,
+               FUN = transf_seven_scale)
+}
